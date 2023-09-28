5779cc7e7c3d8b6feed8cbd3b455d4333fdb1057
Markus Lange <markus@hybrosia.com>
2023-09-28 11:23:36 +0200

    Added PlayerHealthManager to GithubTest

--- Assets/Scripts/GithubTest.cs
+++ Assets/Scripts/GithubTest.cs
@@ -1,13 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GithubTest : MonoBehaviour
 {
+    public PlayerHealthManager _playerHealthManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // DO Something with player Health
     }
 
     // Update is called once per frame
